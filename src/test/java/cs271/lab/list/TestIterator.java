f70c643530503fc030aebdb1c093d77ffb7bd60e
Luke Dosen <lddosen@gmail.com>
2018-06-15 14:38:53 -0500

    TestIterator completed

--- src/test/java/cs271/lab/list/TestIterator.java
+++ src/test/java/cs271/lab/list/TestIterator.java
@@ -6,6 +6,7 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 import org.junit.After;
@@ -47,19 +48,18 @@ public class TestIterator {
     final Iterator<Integer> i = list.iterator();
     assertTrue(i.hasNext());
     assertEquals(33, i.next().intValue());
-    // TODO fix the expected values in the assertions below
     assertTrue(i.hasNext());
-    assertEquals(0, i.next().intValue());
+    assertEquals(77, i.next().intValue());
     assertTrue(i.hasNext());
-    assertEquals(0, i.next().intValue());
+    assertEquals(44, i.next().intValue());
     assertTrue(i.hasNext());
-    assertEquals(0, i.next().intValue());
+    assertEquals(77, i.next().intValue());
     assertTrue(i.hasNext());
-    assertEquals(0, i.next().intValue());
+    assertEquals(55, i.next().intValue());
     assertTrue(i.hasNext());
-    assertEquals(0, i.next().intValue());
+    assertEquals(77, i.next().intValue());
     assertTrue(i.hasNext());
-    assertEquals(0, i.next().intValue());
+    assertEquals(66, i.next().intValue());
     assertFalse(i.hasNext());
   }
 
@@ -75,13 +75,13 @@ public class TestIterator {
     final Iterator<Integer> i = list.iterator();
     while (i.hasNext()) {
       if (i.next() == 77) {
+        //list.remove(Integer.valueOf(77)); //doesn't work correctly because it's not using the iterator
         i.remove(); // TODO what happens if you use list.remove(Integer.valueOf(77))?
       }
     }
-    // TODO using assertEquals and Arrays.asList, express which values are left in the list
-    // See TestList.java for examples of how to use Arrays.asList; also see the Java Arrays
-    // class for more information
-    fail("Not yet implemented"); // remove this line when done
+    Integer[] b = new Integer[] {33, 44, 55, 66};
+    assertEquals(true, list.containsAll(Arrays.asList(b)));
+    // TODO clarify if this is the correct way to test
   }
 
   @Test
@@ -95,9 +95,11 @@ public class TestIterator {
     list.add(66);
     double sum = 0;
     int n = 0;
-    // TODO use an iterator and a while loop to compute the average (mean) of the values
-    // (defined as the sum of the items divided by the number of items)
-    // testNonempty shows how to use an iterator; use i.hasNext() in the while loop condition
+    final Iterator<Integer> i = list.iterator();
+    while (i.hasNext()){
+      sum = sum + i.next();
+      n++;
+      }
     assertEquals(61.3, sum / n, 0.1);
     assertEquals(7, n);
   }
